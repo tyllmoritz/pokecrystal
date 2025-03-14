17ce476e3679ca813f0f603fd2b1d3e221ccc7da
Robin Bertram <git@robin.bertram.id>
2025-03-14 16:57:28 +0100

    move stuff: free space in last 4 Banks

--- main.asm
+++ main.asm
@@ -683,7 +683,7 @@ INCLUDE "engine/events/battle_tower/load_trainer.asm"
 INCLUDE "engine/events/odd_egg.asm"
 
 
-SECTION "Stadium 2 Checksums", ROMX[$7DE0], BANK[$7F]
+SECTION "Stadium 2 Checksums", ROMX
 
 ; The end of the ROM is taken up by checksums of the content, apparently used
 ; by Pokémon Stadium 2 due to the checksums' "N64PS3" header. (In Japan,
